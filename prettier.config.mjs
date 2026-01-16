c49fab0111ead2408356bfdeee60a57f9663ef8e
RookieAND <gwangin1999@naver.com>
2026-01-17 02:03:47 +0900

    chore: prettier.config.js 기본 설정 파일 추가

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
+    trailingComma: 'all',
+    tabWidth: 4,
+    useTabs: true,
+    semi: true,
+    singleQuote: true,
+}
+
+export default config;
