54f37eff938fabd84bd1c42ac6f4fcdafd3c0c01
RookieAND <gwangin1999@naver.com>
2026-01-25 23:28:06 +0900

    fix: Intro 작업에 아직 포함하면 안될 파일들을 일괄 제거

--- app/gathering/[gatheringId]/opinion/page.tsx
+++ app/gathering/[gatheringId]/opinion/page.tsx
@@ -1,35 +1,21 @@
 "use client";
 
-import { useRouter, useParams } from "next/navigation";
-import { FormProvider } from "react-hook-form";
+import { useParams } from "next/navigation";
 import { useMemo } from "react";
 
-import {
-	IntroStep,
-	DistanceStepContent,
-	DistanceStepFooter,
-	DislikeStepContent,
-	DislikeStepFooter,
-	PreferenceStepContent,
-	PreferenceStepFooter,
-	StepTransition,
-} from "#/pageComponents/gathering/opinion";
-import { useOpinionForm, useOpinionFunnel } from "#/hooks/gathering";
+import { IntroStep } from "#/pageComponents/gathering/opinion";
+import { useOpinionFunnel } from "#/hooks/gathering";
 import { Button } from "#/components/button";
 import { Layout } from "#/components/layout";
-import { BackwardButton } from "#/components/backwardButton/BackwardButton";
 import {
 	MOCK_MEETING_DATA,
 	type MeetingContext,
 } from "#/constants/gathering/opinion/meeting";
 
 export default function OpinionPage() {
-	const router = useRouter();
 	const params = useParams();
 	const gatheringId = params.gatheringId as string;
-
-	const form = useOpinionForm();
-	const { step, direction, next, back, isFirstStep } = useOpinionFunnel();
+	const { step, next } = useOpinionFunnel();
 
 	// Meeting context
 	const meetingContext = useMemo<MeetingContext>(
@@ -40,20 +26,6 @@ export default function OpinionPage() {
 		}),
 		[gatheringId],
 	);
-
-	const handleBackward = () => {
-		if (isFirstStep) {
-			router.push(`/gathering/${gatheringId}`);
-		} else {
-			back();
-		}
-	};
-
-	const handleComplete = () => {
-		const data = form.getValues();
-		console.log("Opinion submitted:", data);
-		router.push(`/gathering/${gatheringId}/opinion/complete`);
-	};
 
 	// Intro step - special layout
 	if (step === "intro") {
@@ -79,47 +51,4 @@ export default function OpinionPage() {
 			</Layout.Root>
 		);
 	}
-
-	// Survey steps - with FormProvider
-	const renderContent = () => {
-		switch (step) {
-			case "distance":
-				return <DistanceStepContent meetingContext={meetingContext} />;
-			case "dislike":
-				return <DislikeStepContent />;
-			case "preference":
-				return <PreferenceStepContent />;
-			default:
-				return null;
-		}
-	};
-
-	const renderFooter = () => {
-		switch (step) {
-			case "distance":
-				return <DistanceStepFooter onNext={next} />;
-			case "dislike":
-				return <DislikeStepFooter onNext={next} />;
-			case "preference":
-				return <PreferenceStepFooter onComplete={handleComplete} />;
-			default:
-				return null;
-		}
-	};
-
-	return (
-		<FormProvider {...form}>
-			<Layout.Root>
-				<Layout.Header>
-					<BackwardButton onClick={handleBackward} />
-				</Layout.Header>
-				<Layout.Content>
-					<StepTransition step={step} direction={direction}>
-						{renderContent()}
-					</StepTransition>
-				</Layout.Content>
-				{renderFooter()}
-			</Layout.Root>
-		</FormProvider>
-	);
 }
