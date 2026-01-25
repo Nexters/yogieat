52466a958fe0387b147897948f6b044ab8378210
RookieAND <gwangin1999@naver.com>
2026-01-25 23:01:22 +0900

    feat: setup opinion component exports
    - Create opinion index.ts
    - Export IntroStep

---
+++ src/pageComponents/gathering/opinion/index.ts
@@ -0,0 +1,17 @@
+export { IntroStep } from "./IntroStep";
+export {
+	DistanceStepContent,
+	DistanceStepFooter,
+} from "./DistanceStep";
+export {
+	DislikeStepContent,
+	DislikeStepFooter,
+} from "./DislikeStep";
+export {
+	PreferenceStepContent,
+	PreferenceStepFooter,
+} from "./PreferenceStep";
+export { CompleteView } from "./CompleteView";
+export { SubmissionBottomSheet } from "./SubmissionBottomSheet";
+export { StepTransition } from "./StepTransition";
+export { FoodCategoryButton } from "./FoodCategoryButton";
