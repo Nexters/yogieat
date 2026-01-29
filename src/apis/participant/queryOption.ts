ae4721108efcef1b1b175d602fe31ed8fe2b4a75
RookieAND <gwangin1999@naver.com>
2026-01-30 03:14:11 +0900

    fix: mutationOptions 를 사용하여 옵션을 생성하도록 구조 개선

--- src/apis/participant/queryOption.ts
+++ src/apis/participant/queryOption.ts
@@ -1,14 +1,16 @@
 import { participantKeys } from "./queryKey";
 import { createParticipant } from "./api";
 import type { CreateParticipantRequest } from "./type";
+import { mutationOptions } from "@tanstack/react-query";
 
 /**
  * 참가자 관련 Query/Mutation Options Factory
  */
 export const participantOptions = {
-	create: () => ({
-		mutationKey: participantKeys.create(),
-		mutationFn: (request: CreateParticipantRequest) =>
-			createParticipant(request),
-	}),
+	create: () =>
+		mutationOptions({
+			mutationKey: participantKeys.create(),
+			mutationFn: (request: CreateParticipantRequest) =>
+				createParticipant(request),
+		}),
 };
