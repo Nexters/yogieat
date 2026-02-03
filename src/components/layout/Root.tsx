c7d8965998c3129c148ba2ffac8e788c4efd8630
위영민 <youngminieo1005@gmail.com>
2026-02-04 00:53:07 +0900

    feat: global Layout Root 컴포넌트에 box shadow 적용

--- src/components/layout/Root.tsx
+++ src/components/layout/Root.tsx
@@ -6,7 +6,7 @@ interface RootProps {
 
 export const Root = ({ children }: RootProps) => {
 	return (
-		<div className="ygi:relative ygi:mx-auto ygi:min-h-screen-dynamic ygi:w-full ygi:max-w-root-layout">
+		<div className="ygi:relative ygi:mx-auto ygi:min-h-screen-dynamic ygi:w-full ygi:max-w-root-layout ygi:shadow-drop">
 			{children}
 		</div>
 	);
