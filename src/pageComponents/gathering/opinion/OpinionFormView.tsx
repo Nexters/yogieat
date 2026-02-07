1dd5d612a0a1b49d7295e578006925d65cd601e4
위영민 <youngminieo1005@gmail.com>
2026-02-07 19:59:47 +0900

    chore: remove un-used

--- src/pageComponents/gathering/opinion/OpinionFormView.tsx
+++ src/pageComponents/gathering/opinion/OpinionFormView.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { redirect, useParams, useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { FormProvider } from "react-hook-form";
 
 import {
