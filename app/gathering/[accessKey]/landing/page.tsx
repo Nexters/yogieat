7cbafa92bba4704cb80f6800960f69d38fe79f6e
RookieAND <gwangin1999@naver.com>
2026-02-24 00:54:56 +0900

    chore: 불필요한 코드 및 import 구문 수정

--- app/gathering/[accessKey]/landing/page.tsx
+++ app/gathering/[accessKey]/landing/page.tsx
@@ -13,16 +13,10 @@ interface LandingPageProps {
 	}>;
 }
 
-/**
- * 의견 수렴 랜딩 페이지 (서버 컴포넌트)
- * - 공유 링크로 접근 시 IntroStep을 보여줌
- * - gathering 데이터를 서버에서 prefetch하여 무한 렌더링 방지
- */
 export default async function LandingPage({ params }: LandingPageProps) {
 	const { accessKey } = await params;
 	const queryClient = new QueryClient();
 
-	// 서버에서 gathering 데이터 미리 가져오기
 	await Promise.all([
 		queryClient.prefetchQuery(gatheringQueryOptions.detail(accessKey)),
 		queryClient.prefetchQuery(gatheringQueryOptions.capacity(accessKey)),
