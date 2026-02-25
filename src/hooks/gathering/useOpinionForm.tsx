5cd8ab56084e7e6aa69aae3ac2632e10d3a184be
RookieAND <gwangin1999@naver.com>
2026-02-26 00:42:56 +0900

    fix: 모임 인원이 다 찼거나 이미 결과가 생성된 경우에 대한 Toast 커스텀 진행

--- src/hooks/gathering/useOpinionForm.tsx
+++ src/hooks/gathering/useOpinionForm.tsx
@@ -1,19 +1,19 @@
 "use client";
 
+import { compact } from "es-toolkit";
+import { useParams, useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+
+import { useCreateParticipant } from "#/hooks/apis";
+import { ToastLinkButton } from "#/pageComponents/gathering/opinion";
 import {
 	opinionFormSchema,
 	distanceRangeToKm,
 	type OpinionFormSchema,
 } from "#/schemas/gathering";
-import { useCreateParticipant } from "../apis/participant";
-import { useParams, useRouter } from "next/navigation";
 import { ERROR_CODES, isApiError } from "#/utils/api";
 import { toast } from "#/utils/toast";
-import { compact } from "es-toolkit";
-import { createElement } from "react";
-import { ArrowLeftIcon } from "#/icons/arrowLeftIcon";
 
 export function useOpinionForm() {
 	const router = useRouter();
@@ -53,29 +53,23 @@ export function useOpinionForm() {
 			router.replace(`/gathering/${accessKey}/opinion/pending`);
 		} catch (error) {
 			if (isApiError(error)) {
-				if (error.errorCode === ERROR_CODES.GATHERING_FULL) {
-					toast.warning(error.message, {
-						action: createElement(
-							"button",
-							{
-								type: "button",
-								className:
-									"ygi:flex ygi:ml-auto ygi:items-center ygi:gap-0.5 ygi:justify-center ygi:cursor-pointer ygi:text-palette-primary-500 ygi:body-14-sb",
-								onClick: () => {
-									router.push(
-										`/gathering/${accessKey}/opinion/result`,
-									);
-								},
-							},
-							"추천 결과 보기",
-							createElement(ArrowLeftIcon, {
-								size: 20,
-								className:
-									"ygi:text-palette-primary-500 ygi:rotate-180",
-							}),
-						),
-					});
-					return;
+				switch (error.errorCode) {
+					case ERROR_CODES.GATHERING_FULL:
+					case ERROR_CODES.RECOMMEND_ALREADY_PROCEEDED: {
+						toast.warning(error.message, {
+							action: (
+								<ToastLinkButton
+									label="추천 결과 보기"
+									onClick={() => {
+										router.push(
+											`/gathering/${accessKey}/opinion/result`,
+										);
+									}}
+								/>
+							),
+						});
+						return;
+					}
 				}
 
 				toast.warning(error.message);
