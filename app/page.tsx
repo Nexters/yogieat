813e208d9625578231093f7203a82ded12d7b089
위영민 <youngminieo1005@gmail.com>
2026-03-04 00:23:44 +0900

    feat: scaffold ServiceLandingPage component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { LandingPage } from "#/pageComponents/landing";
+import { ServiceLandingPage } from "#/pageComponents/serviceLanding";
 
 export default function Home() {
-	return <LandingPage />;
+	return <ServiceLandingPage />;
 }
---
+++ src/pageComponents/serviceLanding/ServiceLandingPage.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export const ServiceLandingPage = () => {
+	return (
+		<div className="ygi:w-full ygi:overflow-x-hidden">
+			{/* Sections will be added here */}
+		</div>
+	);
+};
